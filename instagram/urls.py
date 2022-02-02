44cf3d6be29f9a1f48490f979f53e17dd42c1eb4
engineer-km <evolvekm123@gmail.com>
2022-02-02 13:24:52 +0900

    80강 JSON 응답뷰 만들기

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)  # 2개의 URL 를 만들어줌.
+# router.urls  # url pattern list
 
 
 urlpatterns = [
-    
+    path('', include(router.urls))    
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+    '''
+    def dispatch(self, request, *args, **kwargs):
+        print("request.body:", request.body)  #print 비추천, logger 추천
+        print("request.POST:", request.POST)  #print 비추천, logger  추천
+        return super().dispatch(request, *args, **kwargs)
+    '''
+
+'''
+def post_list(request):
+    # request.method #=> 2개 분기
+    pass
+
+
+def post_detail(request):
+    # request.method #=> 3개 분기
+    pass
+'''
