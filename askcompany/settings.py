5699f4f955d3ba21573eb718222c5fb2e78771b5
engineer-km <evolvekm123@gmail.com>
2022-02-04 21:58:36 +0900

    90강 페이징 (Pagination) 처리

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -128,6 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated', # 'rest_framework.permissions.AllowAny',
     ],
